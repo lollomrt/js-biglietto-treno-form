2c74538e7f3deb9bc21ab3905b0442ebc9c2a76d
lollomrt <lorenzomartini.mrt@outlook.it>
2022-11-28 18:52:02 +0100

    completamento struttura calcoli javascript e form html - no innerhtml del risultato (solo console log)

--- js/javascript.js
+++ js/javascript.js
@@ -7,7 +7,29 @@ button_invia.addEventListener("click", function(){
     nome_e_cognome = document.getElementById("nome-e-cognome").value
     console.log(nome_e_cognome)
     km = document.getElementById("numero-kilometri").value
-    console.log(numero-kilometri)
-    eta = document.getElementsById("eta").value
+    console.log(km)
+    eta = document.getElementById("eta").value
     console.log(eta)
+
+    let prezzo = km * 0.21
+    let sconto_over = prezzo * 40 / 100
+    let sconto_minorenni = prezzo * 20 / 100
+    let prezzo_finale;
+
+    //controllo prezzo
+    if (eta === "minorenne") {
+        prezzo_finale = prezzo - sconto_minorenni
+        console.log(prezzo_finale)
+    }
+
+    else if (eta === "over-65") {
+        prezzo_finale = prezzo - sconto_over
+        console.log(prezzo_finale)
+    }
+
+    else {
+        prezzo_finale = prezzo
+        console.log(prezzo_finale)
+    }
+
 })
