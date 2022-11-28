3e90331004032db6913e3a4476d1f8baaada59d3
lollomrt <lorenzomartini.mrt@outlook.it>
2022-11-28 16:03:15 +0100

    revisione

---
+++ js/javascript.js
@@ -0,0 +1,9 @@
+const nome_e_cognome;
+const km;
+const eta;
+
+const button_invia = document.getElementById("invia")
+button_invia.addEventListener("click", function(){
+    nome_e_cognome = document.getElementById("nome-e-cognome").value
+    console.log("nome_e_cognome")
+})
