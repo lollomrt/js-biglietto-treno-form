9324ae556eb3af203bc00040657c1927e8108f96
lollomrt <lorenzomartini.mrt@outlook.it>
2022-11-28 17:43:51 +0100

    revisione Marco

--- js/javascript.js
+++ js/javascript.js
@@ -5,5 +5,9 @@ let eta;
 let button_invia = document.getElementById("invia")
 button_invia.addEventListener("click", function(){
     nome_e_cognome = document.getElementById("nome-e-cognome").value
-    console.log("nome_e_cognome")
+    console.log(nome_e_cognome)
+    km = document.getElementById("numero-kilometri").value
+    console.log(numero-kilometri)
+    eta = document.getElementsById("eta").value
+    console.log(eta)
 })
